253c6e2997d285701bbf4f791ee903f980fedf1b
StaXxXGod <93221430+StaXxXGod@users.noreply.github.com>
2023-11-22 16:25:23 +0200

    Continue-space E - exit / Dialogue

--- Terror Trails/Assets/Scripts/NPCDialogManager.cs
+++ Terror Trails/Assets/Scripts/NPCDialogManager.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 using TMPro;
+using System;
+using System.Collections.Generic;
 
 public class NPCDialogManager : MonoBehaviour
 {
@@ -24,9 +23,37 @@ public class NPCDialogManager : MonoBehaviour
 
     public List<NPCDialogData> npcDialogs = new List<NPCDialogData>();
 
+    private int currentDialogIndex = 0; // Index of the current dialog
+
+    private void Start()
+    {
+        // Adding text for each item in npcDialogs
+        npcDialogs.Add(new NPCDialogData { dialogID = DialogID.Greeting, dialogText = "Hello fckin cunt" });
+        npcDialogs.Add(new NPCDialogData { dialogID = DialogID.Goodbye, dialogText = "pispopa" });
+        npcDialogs.Add(new NPCDialogData { dialogID = DialogID.Question, dialogText = "Go fuckyourself" });
+    }
+
     public void Talk(DialogID dialogID)
     {
         NPCDialogData npcDialog = npcDialogs.Find(d => d.dialogID == dialogID);
-        dialogBoxText.text = npcDialogs[(int)dialogID].dialogText;
+        dialogBoxText.text = npcDialog.dialogText;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Logic for displaying the next dialog
+            currentDialogIndex++;
+            if (currentDialogIndex < npcDialogs.Count)
+            {
+                dialogBoxText.text = npcDialogs[currentDialogIndex].dialogText;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Logic for exiting the dialog
+            dialogBoxText.text = string.Empty; // Clearing the dialog text field
+        }
     }
 }
